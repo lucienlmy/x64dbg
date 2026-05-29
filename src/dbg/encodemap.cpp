353e3a21b7cec411d31698063a87fbce1f4e7e62
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2026-05-29 22:53:18 +0200

    Fix missing zmmword in a few places

--- src/dbg/encodemap.cpp
+++ src/dbg/encodemap.cpp
@@ -215,6 +215,8 @@ duint GetEncodeTypeSize(ENCODETYPE type)
         return 16;
     case enc_ymmword:
         return 32;
+    case enc_zmmword:
+        return 64;
     case enc_real4:
         return 4;
     case enc_real8:
